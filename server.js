e8218ab0be848f4a7136560b2cafad31745869e6
Abdulxoliq-1030 <abdulxoliq1030@gmail.com>
2024-03-09 17:05:40 +0500

    auth codes

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+const bcrypt = require("bcrypt");
+
+app.use(express.json());
+
+const users = [{ name: "Name" }];
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.post("/users", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    console.log(salt);
+    console.log(hashedPassword);
+    const user = { name: req.body.name, password: hashedPassword };
+    users.push(user);
+    res.status(201).send();
+  } catch (err) {
+    res.status(500).send();
+  }
+});
+
+app.listen(3000);
